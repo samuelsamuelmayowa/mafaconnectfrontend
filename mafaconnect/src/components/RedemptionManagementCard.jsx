f263c965a5260e02cf3d5e8068415520e07b2301
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-12-01 03:49:20 +0100

    converint g

--- mafaconnect/src/components/RedemptionManagementCard.jsx
+++ mafaconnect/src/components/RedemptionManagementCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 
